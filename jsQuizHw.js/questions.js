00154a85e1cdc2030a41e24d1911957d7935d312
laurenbmcguire <102077139+laurenbmcguire@users.noreply.github.com>
2022-05-16 18:12:45 -0500

    jsquiz

---
+++ jsQuizHw.js/questions.js
@@ -0,0 +1,111 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which flies a green, white, and orange (in that order) tricolour flag? ?",
+        answer: "Italy",
+        options: [
+            "Ireland",
+            "Spain",
+            "France",
+            "Italy"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Where was the first example of paper money used??",
+        answer: "China",
+        options: [
+            "Turkey",
+            "Greece",
+            "China",
+            "Japan"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who is generally considered the inventor of the motor car?",
+        answer: "Henry Ford",
+        options: [
+            "Henry Ford",
+            "Karl Benz",
+            "Nikola Tesla",
+            "Charles Villa"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What number was the Apollo mission that successfully put a man on the moon for the first time in human history?",
+        answer: "Apollo 11",
+        options: [
+            "Apollo 11",
+            "Apollo 12",
+            "Apollo 13",
+            "Apollo 14"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following languages has the longest alphabet?",
+        answer: "Russian",
+        options: [
+            "Greek",
+            "Russian",
+            "Arabic",
+            "Egyptian"
+        ]
+    },
+{
+    numb: 6,
+        question: "What is the largest US state by land mass?",
+            answer: "Alaska",
+                options: [
+                    "Alaska",
+                    "Hawaii",
+                    "Texas",
+                    "California"
+                ]
+},
+{
+    numb: 7,
+    question: "Which app has the most total users?",
+            answer: "Instagram",
+                options: [
+                    "Instagram",
+                    "Facebook",
+                    "Twitter",
+                    "Snapchat"
+                ]
+},
+{
+    numb: 8,
+    question: ") How many plays do people (generally) believe that Shakespeare wrote?",
+            answer: "37",
+                options: [
+                    "27",
+                    "37",
+                    "47",
+                    "57"
+                ]
+},
+{
+    numb: 9,
+    question: "Which of the following was considered one of the Seven Ancient Wonders??",
+    answer: "Colossus of Rhodes",
+                options: [
+                    "Colossus of Rhodes",
+                    "Great Wall of China",
+                    "Colosseum",
+                    "The Great Barrier Reefs"
+                ]
+},
+{
+    numb: 10,
+    question: " What is the strongest muscle in the human body?",
+            answer: "Jaw",
+                options: [
+                    "Jaw",
+                    "Heart",
+                    "Glutes",
+                    "Biceps"
+                ]
+},]
